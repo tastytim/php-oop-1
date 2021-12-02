3de12eab722931f02ddc2f36e2b6a4d1e5be52e8
tastytim <tastytimgm@gmail.com>
2021-12-02 14:59:09 +0100

    commit 2

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 
 class Movie {
 
-    public $title="";
-    public $year = "";
+    public $title,$year;
    
     function __construct($title_value, $year_value){
            $this->title = $title_value;
            $this->year = $year_value;
+    }
+
+    function one_value_construct($title_value){
+        $this->title = $title_value;
+        $this->year = 0;
     }
 
     function getTitle(){
@@ -26,6 +30,10 @@ class Movie {
         $this->year=$newYear;
     }
 
+    function MovieAndYear(){
+        return $this->title . " " . $this->year . "<br>";
+    }
+
 };
 
 
@@ -33,12 +41,15 @@ $newMovie = new Movie("New Movie 1", 2021);
 $newMovie1 = new Movie("New Movie 2", 2022); 
 
 
-echo $newMovie->getTitle() . "<br>";
-echo $newMovie->getYear() . "<br>";
-echo $newMovie1->getTitle() . "<br>";
-echo $newMovie1->getYear() . "<br>";
+// echo $newMovie->getTitle() . "<br>";
+// echo $newMovie->getYear() . "<br>";
+// echo $newMovie1->getTitle() . "<br>";
+// echo $newMovie1->getYear() . "<br>";
 
-var_dump($newMovie);
-var_dump($newMovie1);
+// var_dump($newMovie);
+// var_dump($newMovie1);
+
+echo $newMovie->MovieAndYear();
+echo $newMovie1->MovieAndYear();
 
 ?>
